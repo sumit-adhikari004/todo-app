d7cf9281cef7020738a3744426b2001e0b887517
sumit <adhikarisumit004@gmail.com>
2026-06-07 17:24:46 +0900

    Fix Render deployment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static("public"));
